bd4a617dede4f693178d57fcfc620d32b6bc669c
sychyow <sychyow@outlook.com>
2018-09-15 18:29:20 +0300

    Show us the extras if any

--- app/src/main/java/org/supremus/sych/helloworld/MainActivity.java
+++ app/src/main/java/org/supremus/sych/helloworld/MainActivity.java
@@ -2,6 +2,7 @@ package org.supremus.sych.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView title = findViewById(R.id.title_id);
+        Bundle ext = this.getIntent().getExtras();
+        if (ext==null) return;
+        StringBuilder sb = new StringBuilder();
+        for (String key: ext.keySet()) {
+            sb.append(key);
+            sb.append(":");
+            sb.append(ext.get(key));
+            sb.append("\n");
+        }
+        title.setText(sb.toString());
     }
 }
